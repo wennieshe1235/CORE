1d948d4071175c6599638130b7da1bf41a2c8835
wennie <wennieshe1235@gmail.com>
2024-07-27 21:50:32 +0800

    商品資訊_js_tab切換欄功能完成

--- js/product-details.js
+++ js/product-details.js
@@ -1,8 +1,9 @@
-//點擊'+' '-'按鍵，控制商品購買的數量input增加或減少
 
 //確保你的 JavaScript 在 DOM 完全加載後執行。例如，可以使用 DOMContentLoaded 事件：
 document.addEventListener('DOMContentLoaded', function() {
-    const minus = document.querySelector('#minus')
+
+//點擊'+' '-'按鍵，控制商品購買的數量input增加或減少
+const minus = document.querySelector('#minus')
 const plus = document.querySelector('#plus')
 const input = document.querySelector('#input')
 
@@ -27,4 +28,29 @@ plus.addEventListener('click', function(){
     }
     
 });
+
+//下方商品敘述、model資訊，tab欄位點擊切換
+const as = document.querySelectorAll('.detail-span > span')
+
+for(let i = 0; i < as.length; i++){
+    as[i].addEventListener('click', function(){
+        //移除原有active
+    document.querySelector('.detail-span  span.active').classList.remove('active')
+    //新增active給點擊的項目
+    this.classList.add('active')
+
+    //下方資訊一一對應
+    document.querySelector('.detail-content .scope .binding.active').classList.remove('active')
+    document.querySelector(`.detail-content .scope .binding:nth-child(${i + 1})`).classList.add('active')
+    
+    })
+    
+    
+}
+
+
+
+
+
+
 });
